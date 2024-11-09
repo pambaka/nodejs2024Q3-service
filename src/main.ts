c648cfe2791eeda3370be890db8df16aa2ad99a3
pambaka <pambaka@ya.ru>
2024-11-09 18:23:48 +0100

    refactor: the port value is stored in the .env file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = parseInt(process.env.PORT);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(port, () => {
+    console.log(`The app is listening on port ${port}`);
+  });
 }
 bootstrap();
+
+process.on('uncaughtException', (error: Error) => {
+  console.error(error.message);
+
+  if (error instanceof RangeError)
+    console.log('Please check port value in .env file and restart the app.');
+});
